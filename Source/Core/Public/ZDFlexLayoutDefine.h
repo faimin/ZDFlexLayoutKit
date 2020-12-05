ca8d3b3b71a779e7bfdb957c4c950031e7bde8a2
faimin <fuxianchao@gmail.com>
2020-12-05 11:15:55 +0800

    add enum

--- Source/Core/Public/ZDFlexLayoutDefine.h
+++ Source/Core/Public/ZDFlexLayoutDefine.h
@@ -11,13 +11,17 @@
 #import <Foundation/Foundation.h>
 
 typedef NS_OPTIONS(NSInteger, ZDDimensionFlexibility) {
-    ZDDimensionFlexibilityFlexibleWidth = 1 << 0,
-    ZDDimensionFlexibilityFlexibleHeight = 1 << 1,
+    ZDDimensionFlexibilityFlexibleNone      = 0,
+    ZDDimensionFlexibilityFlexibleWidth     = 1 << 0,
+    ZDDimensionFlexibilityFlexibleHeight    = 1 << 1,
+    ZDDimensionFlexibilityFlexibleAll       = ~0L
 };
 
 // compatible with older versions
 #define YGDimensionFlexibility ZDDimensionFlexibility
+#define YGDimensionFlexibilityFlexibleNone (ZDDimensionFlexibilityFlexibleNone)
 #define YGDimensionFlexibilityFlexibleWidth (ZDDimensionFlexibilityFlexibleWidth)
 #define YGDimensionFlexibilityFlexibleHeight (ZDDimensionFlexibilityFlexibleHeight)
+#define YGDimensionFlexibilityFlexibleAll (ZDDimensionFlexibilityFlexibleAll)
 
 #endif /* ZDFlexLayoutDefine_h */
